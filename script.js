168aca2bda74fe3b2f1a6072e9b12db7df09b98c
giselopez94 <146565781+giselopez94@users.noreply.github.com>
2023-11-02 12:37:56 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const estadoElemento = document.getElementById("estado");
-const estadoSeleccionadoElemento = document.getElementById("estadoSeleccionado");
-
-estadoElemento.addEventListener("change", function () {
-  const estadoSeleccionado = estadoElemento.value;
-  estadoSeleccionadoElemento.textContent = estadoSeleccionado;
-});
